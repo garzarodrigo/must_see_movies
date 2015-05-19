bfd43feef8670a96bfb360f126046bb7d4916625
garzarodrigo <rgarza2016@kellogg.northwestern.edu>
2015-05-18 19:46:49 -0500

    class

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,12 @@
 class Role < ActiveRecord::Base
+
+  belongs_to :actor
+  # can be used if your table is called the same and the foreign key also carries the same name
+
+  # belongs_to :actor, :class_name => "Actor", :foreign_key => "actor_id"
+
+
+
+  belongs_to :movie, :class_name => "Movie", :foreign_key => "movie_id"
+
 end
